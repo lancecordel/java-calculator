31b33af4dd567fc4f7d5508b69fdc7483fd76269
Lance Wiltshire <lancewiltshire@Lances-iMac.local>
2022-06-26 16:01:00 -0400

    2nd

--- src/Calculator.java
+++ src/Calculator.java
@@ -26,14 +26,52 @@ public class Calculator implements ActionListener{
 		textfield = new JTextField();
 		textfield.setBounds(50, 25, 300, 50);					//x, y, w, h
 		textfield.setFont(myFont);								//add myFont to text field
-		frame.add(textfield);									//add text field to frame				
+		textfield.setEditable(false);
 		
+		addButton = new JButton("+");							// create instances of JButtons
+		subButton = new JButton("-");
+		mulButton = new JButton("*");
+		divButton = new JButton("/");
+		decButton = new JButton(".");
+		equButton = new JButton("=");
+		delButton = new JButton("del");
+		clrButton = new JButton("C");
+		
+		functionButtons[0] = addButton;							// add buttons to array
+		functionButtons[1] = subButton;
+		functionButtons[2] = mulButton;
+		functionButtons[3] = divButton;
+		functionButtons[4] = decButton;
+		functionButtons[5] = equButton;
+		functionButtons[6] = delButton;
+		functionButtons[7] = clrButton;
+		
+		for(int i = 0; i < 8; i++) {		//iterate through function array
+			functionButtons[i].addActionListener(this);			//and set attributes
+			functionButtons[i].setFont(myFont);
+			functionButtons[i].setFocusable(false);
+		}
+		
+		for(int i = 0; i < numberButtons.length; i++) {			//iterate through buttons array
+			numberButtons[i] = new JButton(String.valueOf(i));
+			numberButtons[i].addActionListener(this);
+			numberButtons[i].setFont(myFont);
+			numberButtons[i].setFocusable(false);
+		}
+		
+		delButton.setBounds(50, 430, 145, 50);
+		clrButton.setBounds(205,430,145, 50);
+		
+		frame.add(delButton);
+		frame.add(clrButton);
+		frame.add(textfield);									//add text field to frame				
 		frame.setVisible(true);
 	}
 	
 	//this is the main where everything happens in the program
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
+		System.out.println("hello, How are you Friend?");
 
 	}
 
